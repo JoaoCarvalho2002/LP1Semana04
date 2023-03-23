f659d06169a8c39ac5239385700a3d5bf901c67a
JoaoCarvalho2002 <jonitkick@gmail.com>
2023-03-23 16:20:03 +0000

    Exercicio 4

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,6 +4,15 @@ namespace Power2Method
 {
     class Program
     {
+        public static void PowersOf2UntilN(int n){
+
+            for(int i =1; i<=(1<<n);i=i<<1)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
         public static void PowersOf2Until5()
         {
             for(int i=1;i<=(1 << 5);i << 1){
@@ -13,8 +22,12 @@ namespace Power2Method
         }
         static void Main(string[] args)
         {
+            int n=0;
             PowersOf2Until5();
             PowersOf2Until5();
+
+            PowersOf2UntilN(n);
+            PowersOf2UntilN(n+1);
         }
     }
 }
